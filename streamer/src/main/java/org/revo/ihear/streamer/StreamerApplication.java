174379e4060c512bfeca1a4ebf9db711f7a28b36
ashraf <ashraf1abdelrasool@gmail.com>
2019-08-18 13:43:46 +0200

    streamer

--- streamer/src/main/java/org/revo/ihear/streamer/StreamerApplication.java
+++ streamer/src/main/java/org/revo/ihear/streamer/StreamerApplication.java
@@ -1,6 +1,5 @@
 package org.revo.ihear.streamer;
 
-import org.revo.base.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +8,6 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.messaging.Message;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -18,17 +16,13 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 import static org.springframework.security.core.authority.AuthorityUtils.createAuthorityList;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.revo.base", "org.revo.ihear.streamer"})
 @EnableDiscoveryClient
 @EnableWebFluxSecurity
 @EnableBinding(Sink.class)
@@ -53,12 +47,6 @@ public class StreamerApplication {
                     }
                 }))
                 .and().and().build();
-    }
-
-
-    @Bean
-    public RouterFunction<ServerResponse> routes(UserService userService) {
-        return route().GET("/", serverRequest -> ServerResponse.ok().body(userService.current(), String.class)).build();
     }
 
     @StreamListener(Sink.INPUT)
