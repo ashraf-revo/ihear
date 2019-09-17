e79c903318c17e0378163f55ab32fabeec5db59d
ashraf <ashraf1abdelrasool@gmail.com>
2019-09-17 18:55:17 +0200

    configure scalable websocket microservice

--- ws/src/main/java/org/revo/ihear/ws/config/Impl/WebSocketTemplateImpl.java
+++ ws/src/main/java/org/revo/ihear/ws/config/Impl/WebSocketTemplateImpl.java
@@ -3,20 +3,28 @@ package org.revo.ihear.ws.config.Impl;
 import org.revo.ihear.ws.config.WebSocketTemplate;
 import org.revo.ihear.ws.config.domain.WSMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.UnicastProcessor;
 
 @Service
 public class WebSocketTemplateImpl implements WebSocketTemplate {
     @Autowired
     private Processor processor;
-//    @Autowired
-//    private UnicastProcessor<Message<WSMessage>> wsMessages;
+    @Autowired
+    private UnicastProcessor<Message<WSMessage>> wsMessages;
 
     @Override
     public void send(Message<WSMessage> wsMessage) {
         this.processor.output().send(wsMessage);
 //        this.wsMessages.onNext(wsMessage);
+    }
+
+    @StreamListener(Sink.INPUT)
+    public void handle(Message<WSMessage> message) {
+        this.wsMessages.onNext(message);
     }
 }
--- ws/src/main/java/org/revo/ihear/ws/config/WebSocketConfig.java
+++ ws/src/main/java/org/revo/ihear/ws/config/WebSocketConfig.java
@@ -1,9 +1,6 @@
 package org.revo.ihear.ws.config;
 
 import org.revo.ihear.ws.config.domain.WSMessage;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +12,6 @@ import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
 import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.UnicastProcessor;
 
 import java.util.HashMap;
@@ -23,17 +19,10 @@ import java.util.Map;
 
 @Configuration
 public class WebSocketConfig {
-    @Autowired
-    private ReactiveJwtDecoder reactiveJwtDecoder;
-    @Autowired
-    private ApplicationEventPublisher applicationEventPublisher;
-    @Autowired
-    private UnicastProcessor<Message<WSMessage>> wsMessages;
-
     @Bean
-    public HandlerMapping handlerMapping() {
+    public HandlerMapping handlerMapping(ReactiveJwtDecoder reactiveJwtDecoder, ApplicationEventPublisher applicationEventPublisher, UnicastProcessor<Message<WSMessage>> wsMessages) {
         Map<String, WebSocketHandler> map = new HashMap<>();
-        map.put("/ws", new WsSocketHandler(reactiveJwtDecoder, applicationEventPublisher,wsMessages));
+        map.put("/ws", new WsSocketHandler(reactiveJwtDecoder, applicationEventPublisher, wsMessages));
         SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
         mapping.setUrlMap(map);
         mapping.setOrder(-1);
@@ -50,10 +39,6 @@ public class WebSocketConfig {
         return new WebSocketSessionRegistry();
     }
 
-    @StreamListener(Sink.INPUT)
-    public void handle(Message<WSMessage> message) {
-        this.wsMessages.onNext(message);
-    }
     @Bean
     public UnicastProcessor<Message<WSMessage>> processor() {
         return UnicastProcessor.create();
