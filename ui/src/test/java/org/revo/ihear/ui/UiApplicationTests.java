564be5b18c9b8635a7a6746c807a85fda140d393
ashraf <ashraf1abdelrasool@gmail.com>
2019-12-27 23:07:11 +0200

    upgrade to 2.2.2.RELEASE version and Hoxton.SR1

--- ui/src/test/java/org/revo/ihear/ui/UiApplicationTests.java
+++ ui/src/test/java/org/revo/ihear/ui/UiApplicationTests.java
@@ -1,16 +1,35 @@
 package org.revo.ihear.ui;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.server.UnAuthenticatedServerOAuth2AuthorizedClientRepository;
+import org.springframework.web.reactive.function.client.WebClient;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class UiApplicationTests {
+    @Autowired
+    private ReactiveClientRegistrationRepository clientRegistrationRepository;
 
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws InterruptedException {
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(
+                clientRegistrationRepository, new UnAuthenticatedServerOAuth2AuthorizedClientRepository());
+        oauth.setDefaultClientRegistrationId("micro");
+        WebClient client = WebClient.builder().filter(oauth).build();
+        String url = "http://localhost:9999/auth/user";
+
+
+        client.get().uri(url).retrieve()
+                .bodyToMono(String.class)
+                .subscribe(it -> {
+                    System.out.println(it);
+                }, error -> {
+                    System.out.println(error.getMessage());
+                });
+        Thread.sleep(2000);
     }
 
 }
