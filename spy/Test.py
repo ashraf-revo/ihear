e5ce1d673001ba71cd1aa05363a88ad740efc2f7
ashraf <ashraf1abdelrasool@gmail.com>
2020-02-15 18:01:22 +0200

    rebase code

--- spy/Test.py
+++ spy/Test.py
@@ -3,6 +3,8 @@ import psutil
 import json
 import requests
 from oauth2_client.credentials_manager import CredentialManager, ServiceInformation
+from aiortc import RTCPeerConnection, RTCSessionDescription
+from aiortc.contrib.media import MediaPlayer
 import requests
 import asyncio
 import json
--- spy/install.sh
+++ spy/install.sh
@@ -2,7 +2,6 @@
 sudo add-apt-repository -y ppa:jonathonf/ffmpeg-4
 sudo apt-get update
 sudo apt-get install ffmpeg
-ffmpeg -version
 sudo apt-get install libavdevice-dev
 sudo apt-get install libavfilter-dev
 sudo apt-get install libopus-dev
