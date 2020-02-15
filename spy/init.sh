6b4f6bcface659e21625fadf26ed3af7171d1e8a
ashraf <ashraf1abdelrasool@gmail.com>
2020-02-15 15:04:02 +0200

    rebase code

--- spy/init.sh
+++ spy/init.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get install ffmpeg
-sudo apt-get install libavdevice-dev libavfilter-dev libopus-dev libvpx-dev pkg-config
+sudo apt-get install libavdevice-dev
+sudo apt-get install libavfilter-dev
+sudo apt-get install libopus-dev
+sudo apt-get install libvpx-dev
+sudo apt-get install pkg-config
