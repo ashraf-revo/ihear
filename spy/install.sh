c47a2c0e1de54f12977fb9094df8b4c5aa65cd31
ashraf <ashraf1abdelrasool@gmail.com>
2020-02-29 15:55:25 +0200

    adding pre.sh

--- spy/install.sh
+++ spy/install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 sudo add-apt-repository -y ppa:jonathonf/ffmpeg-4
 sudo apt-get update
-sudo apt-get install ffmpeg
-sudo apt-get install libavdevice-dev
-sudo apt-get install libavfilter-dev
-sudo apt-get install libopus-dev
-sudo apt-get install libvpx-dev
-sudo apt-get install pkg-config
+sudo apt-get install -y ffmpeg
+ffmpeg -version
+sudo apt-get install -y libavdevice-dev
+sudo apt-get install -y libavfilter-dev
+sudo apt-get install -y libopus-dev
+sudo apt-get install -y libvpx-dev
+sudo apt-get install -y pkg-config
+sudo apt-get install -y libsrtp2-dev
+sudo apt-get install -y libffi-dev
