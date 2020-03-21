6db9a93ceaac785b818c0eeca78101e6937dc7ed
ashraf <ashraf1abdelrasool@gmail.com>
2020-03-21 16:59:01 +0200

    add pivotal support

--- ui/src/main/java/org/revo/ihear/ui/UiApplication.java
+++ ui/src/main/java/org/revo/ihear/ui/UiApplication.java
@@ -3,13 +3,11 @@ package org.revo.ihear.ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.java.ServiceScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan(basePackages = {"org.revo.ihear.service.auth", "org.revo.ihear.ui"})
-@ServiceScan
 public class UiApplication {
 
     public static void main(String[] args) {
